4e6f6b81a629df19ed55575e44bb905863a2d513
Onida <OnidaSurshana@gmail.com>
2019-07-24 16:18:22 -0400

    did all the html i should need

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var imgs = ["img\bag.jpg", 
+            "img\banana.jpg", 
+            "img\bathroom.jpg", 
+            "img\boots.jpg", 
+            "img\breakfast.jpg", 
+            "img\bubblegum.jpg", 
+            "img\chair.jpg"];
